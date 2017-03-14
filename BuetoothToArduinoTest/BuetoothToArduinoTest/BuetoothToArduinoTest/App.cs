7ef8e85ea78940fbcd43636b42dffd9df34214f9
Rob <robertcmolidor@gmail.com>
2017-03-14 01:05:57 -0500

    initial commit

---
+++ BuetoothToArduinoTest/BuetoothToArduinoTest/BuetoothToArduinoTest/App.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+
+namespace BuetoothToArduinoTest
+{
+    public class App : Application
+    {
+        public App()
+        {
+            
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            MainPage = new BuetoothToArduinoTest.MainPage();
+        }
+        
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
